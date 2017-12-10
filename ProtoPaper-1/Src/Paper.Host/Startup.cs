0d1b780f7c896000c7b91fc7db534eac029b998f
Guga Coder <guga.coder@gmail.com>
2017-12-09 22:33:31 -0200

    Criado o módulo PaperStatic

--- ProtoPaper-1/Src/Paper.Host/Startup.cs
+++ ProtoPaper-1/Src/Paper.Host/Startup.cs
@@ -1,13 +1,23 @@
 ﻿using System;
+using System.Net;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.Extensions.DependencyInjection;
+using Paper.Modules.Static.Controllers;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Paper.Modules.Static.Extensions;
 
 namespace Paper.Host
 {
@@ -23,7 +33,8 @@ namespace Paper.Host
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddApplicationInsightsTelemetry(Configuration);
-      services.AddMvc();
+      services.AddMvc()
+        .AddPaperStaticModule();
     }
 
     public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
@@ -33,12 +44,15 @@ namespace Paper.Host
 
       if (env.IsDevelopment())
       {
-        app.UseDeveloperExceptionPage();
         app.UseBrowserLink();
       }
-      
+
+      app.UseStatusCodePagesWithReExecute("/error/{0}");
+      app.UseExceptionHandler("/error/500");
+
       app.UseStaticFiles();
       app.UseDirectoryBrowser();
+
       app.UseMvc();
     }
   }
